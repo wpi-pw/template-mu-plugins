78dcd581f6b0f5414dee4333bcc846f1f234b944
Dima Minka <bzx@mail.ru>
2019-06-19 13:03:30 +0300

    feat--init--wp-cli and bedrock support

--- mu-plugins-init.sh
+++ mu-plugins-init.sh
@@ -6,12 +6,32 @@
 
 source ${PWD}/lib/app-init.sh
 
-version=""
-zip="^(https|git)(:\/\/|@)([^\/:]+)[\/:]([^\/:]+)\/([^\/:]+)\/([^\/:]+)\/(.+).zip$"
-# Get all single plugins and run install by type
+# Get all mu plugins and run install
 for i in "${!conf_app_mu_plugins__name[@]}"
 do
     printf "${GRN}======================================================${NC}\n"
-    printf "${GRN}Installing plugin ${conf_app_mu_plugins__name[$i]}${NC}\n"
+    printf "${GRN}Installing mu plugin ${conf_app_mu_plugins__name[$i]} ${NC}\n"
     printf "${GRN}======================================================${NC}\n"
+    # define var for mu plugin path
+    mu_plugins_path=${PWD}
+    # Check workflow type
+    if [ "${conf_app_setup_workflow}" == "wp-cli" ]; then
+      # mu plugins path
+      mu_plugins_path=${PWD}/web/wp-content/mu-plugins
+      # Check if mu-directory don't exist
+      if [ ! -d "$mu_plugins_path" ]; then
+        # mu plugins directory making
+        mkdir $mu_plugins_path
+      fi
+    elif [[ $conf_app_setup_workflow == *"bedrock"* ]]; then
+      # mu plugins path
+      mu_plugins_path=${PWD}/web/app/mu-plugins
+      # Check if mu-directory don't exist
+      if [ ! -d "$mu_plugins_path" ]; then
+        # mu plugins directory making
+        mkdir $mu_plugins_path
+      fi
+    fi
+    # get mu-plugin via zip and renaming
+    wget -c ${conf_app_mu_plugins__zip[$i]} -O $mu_plugins_path/${conf_app_mu_plugins__name[$i]}.php
 done
