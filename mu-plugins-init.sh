a1fec2d86ef21744106520f255732fdb521144e8
Dima Minka <bzx@mail.ru>
2019-10-20 00:16:43 +0300

    chore--mu-template--rebuild to wpi done

--- mu-plugins-init.sh
+++ mu-plugins-init.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
 
-# MU Plugins Init - Wp Pro Club
-# by DimaMinka (https://dimaminka.com)
-# https://github.com/wp-pro-club/init
+# WPI MU-Plugins
+# by DimaMinka (https://dima.mk)
+# https://github.com/wpi-pw/app
 
-source ${PWD}/lib/app-init.sh
+# Get config files and put to array
+wpi_confs=()
+for ymls in wpi-config/*
+do
+  wpi_confs+=("$ymls")
+done
+
+# Get wpi-source for yml parsing, noroot, errors etc
+source <(curl -s https://raw.githubusercontent.com/wpi-pw/template-workflow/master/wpi-source.sh)
 
 # Get all mu plugins and run install
-for i in "${!conf_app_mu_plugins__name[@]}"
+for i in "${!wpi_plugins_mu_plugins__name[@]}"
 do
     printf "${GRN}======================================================${NC}\n"
-    printf "${GRN}Installing mu plugin ${conf_app_mu_plugins__name[$i]} ${NC}\n"
+    printf "${GRN}Installing mu plugin ${wpi_plugins_mu_plugins__name[$i]} ${NC}\n"
     printf "${GRN}======================================================${NC}\n"
     # define var for mu plugin path
     mu_plugins_path=${PWD}
     # Check workflow type
-    if [ "${conf_app_setup_workflow}" == "wp-cli" ]; then
+    if [ "${wpi_init_workflow}" == "wp-cli" ]; then
       # mu plugins path
       mu_plugins_path=${PWD}/web/wp-content/mu-plugins
       # Check if mu-directory don't exist
@@ -23,7 +31,7 @@ do
         # mu plugins directory making
         mkdir $mu_plugins_path
       fi
-    elif [[ $conf_app_setup_workflow == *"bedrock"* ]]; then
+    elif [[ $wpi_init_workflow == *"bedrock"* ]]; then
       # mu plugins path
       mu_plugins_path=${PWD}/web/app/mu-plugins
       # Check if mu-directory don't exist
@@ -32,6 +40,6 @@ do
         mkdir $mu_plugins_path
       fi
     fi
-    # get mu-plugin via zip and renaming
-    wget -c ${conf_app_mu_plugins__zip[$i]} -O $mu_plugins_path/${conf_app_mu_plugins__name[$i]}.php
+    # get mu-plugin via php_url and renaming
+    wget -c ${wpi_plugins_mu_plugins__php_url[$i]} -O $mu_plugins_path/${wpi_plugins_mu_plugins__name[$i]}.php
 done
