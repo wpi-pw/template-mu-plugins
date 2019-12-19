6570200a3fb20a5f7a082a9dfefc56632d4ead58
DimaMinka <cdk@cdk.co.il>
2019-12-19 10:44:51 +0200

    chore--mu-plugins-init.sh--rebuild

--- mu-plugins-init.sh
+++ mu-plugins-init.sh
@@ -14,16 +14,20 @@ done
 # Get wpi-source for yml parsing, noroot, errors etc
 source <(curl -s https://raw.githubusercontent.com/wpi-pw/template-workflow/master/wpi-source.sh)
 
+#cur_env=$(cur_env)
+
+# Create array of plugin list and loop
+mapfile -t mu_plugins < <( wpi_yq 'plugins.must_use.[*].name' )
 # Get all mu plugins and run install
-for i in "${!wpi_plugins_mu_plugins__name[@]}"
+for i in "${!mu_plugins[@]}"
 do
-    printf "${GRN}======================================================${NC}\n"
-    printf "${GRN}Installing mu plugin ${wpi_plugins_mu_plugins__name[$i]} ${NC}\n"
-    printf "${GRN}======================================================${NC}\n"
+    printf "${GRN}==========================================================${NC}\n"
+    printf "${GRN}Installing mu plugin $(wpi_yq plugins.must_use.[$i].name) ${NC}\n"
+    printf "${GRN}==========================================================${NC}\n"
     # define var for mu plugin path
     mu_plugins_path=${PWD}
     # Check workflow type
-    if [ "${wpi_init_workflow}" == "wp-cli" ]; then
+    if [ "$(wpi_yq init.workflow)" == "wp-cli" ]; then
       # mu plugins path
       mu_plugins_path=${PWD}/web/wp-content/mu-plugins
       # Check if mu-directory don't exist
@@ -31,7 +35,7 @@ do
         # mu plugins directory making
         mkdir $mu_plugins_path
       fi
-    elif [[ $wpi_init_workflow == *"bedrock"* ]]; then
+    elif [ "$(wpi_yq init.workflow)" == "bedrock" ]; then
       # mu plugins path
       mu_plugins_path=${PWD}/web/app/mu-plugins
       # Check if mu-directory don't exist
@@ -41,5 +45,5 @@ do
       fi
     fi
     # get mu-plugin via php_url and renaming
-    curl --silent ${wpi_plugins_mu_plugins__php_url[$i]} > $mu_plugins_path/${wpi_plugins_mu_plugins__name[$i]}.php
+    curl --silent $(wpi_yq plugins.must_use.[$i].php_url) > $mu_plugins_path/$(wpi_yq plugins.must_use.[$i].name).php
 done
